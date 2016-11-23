4ce60cab5ef4da329973fb9d5d1ca5649b4b0d11
Luis Reinoso <lomejordejr@riseup.net>
2016-11-23 14:37:08 -0500

    Agregado funcionalidad de suma

--- nodejs/app.js
+++ nodejs/app.js
@@ -5,4 +5,13 @@ var numero2 = 2;
 //Impresion
 console.log(numero1 + numero2);
 
-//generacion
+console.log(imprimirSuma1y2());
+
+module.export = {
+    funcionalidad: imprimirSuma1y2()
+}
+
+//suma
+function imprimirSuma1y2 () {
+    return numero1 + numero2;
+}
