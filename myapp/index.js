48cd097fff5f9adf8c3f0d879ff0171173706097
Luis Reinoso <lomejordejr@riseup.net>
2016-11-30 13:47:28 -0500

    Agregado post y get request

--- myapp/index.js
+++ myapp/index.js
@@ -1,8 +1,19 @@
+/** importacion **/
 var express = require('express')
 var app = express()
 
 app.get('/TecnologiasWeb', function (req, res) {
   res.send('Hello World!')
+})
+
+/** get resquest **/
+app.get('/get', function (req, res) {
+    res.send('TecnologiasWeb get')
+})
+
+/** post resquest **/
+app.post('/', function(req, res) {
+    res.send('TecnologiaWeb post')
 })
 
 app.listen(5050, function () {
