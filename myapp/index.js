d9630114034a84669c3f68229116cf816b5ee1cb
Luis Reinoso <lomejordejr@riseup.net>
2016-11-30 13:36:07 -0500

    Agregado cambio de puerto y url

--- myapp/index.js
+++ myapp/index.js
@@ -1,10 +1,10 @@
 var express = require('express')
 var app = express()
 
-app.get('/', function (req, res) {
+app.get('/TecnologiasWeb', function (req, res) {
   res.send('Hello World!')
 })
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.listen(5050, function () {
+  console.log('Example app listening on port 5050!')
 })
