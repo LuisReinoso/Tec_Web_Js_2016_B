1d1e4c375eaf1cbf5c6cffe13216bd1aa88233bb
Luis Reinoso <lomejordejr@riseup.net>
2016-12-07 15:00:18 -0500

    Agregado informacion de usuario 2 usuarios

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express')
 var app = express()
 
 var fs = require("fs");
+var fs2 = require("fs");
 
 var quePasa= '';
 console.log(quePasa = 'esta por leer el archivo')
@@ -22,14 +23,24 @@ console.log(quePasa = 'termino de leer el archivo')
 
 app.get('/', function(req, res) {
     
+    var datos;
     console.log("1 antes de leer")
+    
     fs.readFile('./paginas/pagina.html', 
             'utf8', 
-            function(err, archivoLeido) {
-    console.log(error);
-    console.log(archivoLeido);
-        res.send(archivoLeido);
+            function(err, archivoLeido2) {
+                //console.log(error);
+                console.log(archivoLeido2);
+        
+                fs.readFile('./paginas/usuario.html', 
+                    'utf8', 
+                    function(err, archivoLeido1) {
+                        //console.log(error);
+                        console.log(archivoLeido1);
+                        res.send(archivoLeido1 +archivoLeido2);
+                });
     });
+    
     console.log("2 parece que termino de leer")
 })
 
